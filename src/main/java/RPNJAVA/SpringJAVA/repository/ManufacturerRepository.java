dac354f79b9c7a02730d5839065e04874fd0beb4
Arseni <moiseevsenya2002@gmail.com>
2023-03-12 21:00:16 +0300

    initial commit

--- src/main/java/RPNJAVA/SpringJAVA/repository/ManufacturerRepository.java
+++
@@ -1,8 +0,0 @@
-package RPNJAVA.SpringJAVA.repository;
-
-import RPNJAVA.SpringJAVA.model.Manufacturer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManufacturerRepository extends JpaRepository<Manufacturer, String> {
-
-}
--- src/main/java/RPNJAVA/SpringJAVA/repository/PartRepository.java
+++
@@ -1,7 +0,0 @@
-package RPNJAVA.SpringJAVA.repository;
-
-import RPNJAVA.SpringJAVA.model.Part;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartRepository extends JpaRepository<Part, Long> {
-}
